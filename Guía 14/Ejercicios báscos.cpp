9fa25b473569df018f4a8bb3b25d81628caf5909
Ramiro Menendez <ramiro99@live.com.ar>
2022-11-03 16:32:45 -0300

    listas enlazadas

---
+++ Guía 14/Ejercicios báscos.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+struct Nodo {
+    int info;
+    struct Nodo* sig;
+}
+
+
+//----------------------------------------------
+int main() {
+	
+	
+	
+	return 0;
+}
+//----------------------------------------------
